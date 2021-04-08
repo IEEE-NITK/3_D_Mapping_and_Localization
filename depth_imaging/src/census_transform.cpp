458efc499c6a6b595ad515c436abc2610feb0244
chandravaran <chandravaran2000@gmail.com>
2021-04-08 23:56:19 +0530

    local changes

--- depth_imaging/src/census_transform.cpp
+++ depth_imaging/src/census_transform.cpp
@@ -45,7 +45,7 @@ class CensusTransform
                     {
                         for(int jj=j-mid ; jj<j+mid+1 ; jj++)
                         {
-                            if( imgr.at<uchar>(Point(ii, jj)) < imgr.at<uchar>(Point(i,j)))
+                            if( imgr.at<uchar>(ii, jj) < imgr.at<uchar>(i,j))
                             {
                                 csr[count]=0;
                                 // cout << imgr.data[ii, jj] ;
@@ -80,12 +80,12 @@ class CensusTransform
                     {
                         count=0;
                         cmp  =0;
-                        csl[block_size*block_size]={0};
+                        // csl[block_size*block_size]={0};
                         for(int ii=i-mid ; ii<i+mid+1 ; ii++)
                         {
                             for(int kk=k-mid ; kk<k+mid+1 ; kk++)
                             {
-                                if( imgl.at<uchar>(Point(ii , kk)) < imgl.at<uchar>(Point(i,k)))
+                                if( imgl.at<uchar>(ii , kk) < imgl.at<uchar>(i,k))
                                 {
                                     csl[count]=0;
                                     // cout << csl[count-1] ;
@@ -110,12 +110,13 @@ class CensusTransform
  
                     output_image.at<uchar>(i,j) =(uint8_t) (indexofSmallestElement(error,disparity_levels))*255/disparity_levels;
                     cout << "CSL: "<< endl;
-                    for (size_t i = 0; i < sizeof(csl); i++) {
+                    for (size_t i = 0; i < sizeof(csl[0]); i++) 
+                    {
                         std::cout << csl[i] << ' ';
                     }
                     cout << " " << endl;
                     cout << "CSR: "<< endl;
-                    for (size_t i = 0; i < sizeof(csr); i++) {
+                    for (size_t i = 0; i < sizeof(csr[0]); i++) {
                         std::cout << csr[i] << ' ';
                     }
                     cout << " " << endl;
@@ -136,7 +137,8 @@ class CensusTransform
             for(int i = 1; i < size; i++)
             {
                 if(array[i] < mini)
-                    idx = i;           
+                    idx = i;
+                    mini = array[i];           
             }
 
             return idx;
@@ -147,12 +149,14 @@ int main()
 {
     Mat imgl,imgr,disp_img;
     int kernel_size[2]={5,5};
-    int block_size=5;
+    int block_size=7;
     int disparity_levels=32;
     CensusTransform CT;
 
-    imgl = imread("/home/ubuntu1804/eyantra_ws/src/3_D_Mapping_and_Localization/depth_imaging/src/im2.png" , IMREAD_GRAYSCALE ); // Load an image
-    imgr = imread("/home/ubuntu1804/eyantra_ws/src/3_D_Mapping_and_Localization/depth_imaging/src/im6.png", IMREAD_GRAYSCALE ); // Load an image
+    // imgl = imread("/home/ubuntu1804/eyantra_ws/src/3_D_Mapping_and_Localization/depth_imaging/src/im2.png" , IMREAD_GRAYSCALE ); // Load an image
+    imgl = imread("/home/chandravaran/catkin_ws/src/3_D_Mapping_and_Localization/depth_imaging/src/im2.png" , IMREAD_GRAYSCALE ); // Load an image
+    // imgr = imread("/home/ubuntu1804/eyantra_ws/src/3_D_Mapping_and_Localization/depth_imaging/src/im6.png", IMREAD_GRAYSCALE ); // Load an image
+    imgr = imread("/home/chandravaran/catkin_ws/src/3_D_Mapping_and_Localization/depth_imaging/src/im6.png", IMREAD_GRAYSCALE ); // Load an image
 
     imshow("left image",imgl);
     imshow("right image",imgr);
